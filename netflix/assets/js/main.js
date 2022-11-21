28e4fadffa837c7fea2f5b81abdd708b9cc258f4
Wilson Souto dos Reis <wilsonsdr@gmail.com>
2022-11-21 19:34:33 -0300

    menu responsivo e navbar

---
+++ netflix/assets/js/main.js
@@ -0,0 +1,15 @@
+let contador = 0;
+let main;
+const links = document.querySelector(".links");
+const burguer = document.querySelector(".burguer");
+
+burguer.addEventListener("click", () => {
+  contador++;
+  main = document.querySelector(".main").style.marginTop = "280px";
+  burguer.classList.toggle("active");
+  links.classList.toggle("active");
+
+  if (contador % 2 == 0) {
+    main = document.querySelector(".main").style.marginTop = "110px";
+  }
+});
