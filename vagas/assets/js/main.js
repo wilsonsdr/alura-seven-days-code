3ff915b87fc42a927897af8946770d467312ce99
Wilson Souto dos Reis <wilsonsdr@gmail.com>
2022-11-21 17:26:30 -0300

    menu responsivo e section about

---
+++ vagas/assets/js/main.js
@@ -0,0 +1,15 @@
+let about;
+let contador = 0;
+const links = document.querySelector(".links");
+const burguer = document.querySelector(".burguer");
+
+burguer.addEventListener("click", () => {
+  contador++;
+  about = document.querySelector(".about").style.marginTop = "230px";
+  burguer.classList.toggle("active");
+  links.classList.toggle("active");
+
+  if (contador % 2 == 0) {
+    about = document.querySelector(".about").style.marginTop = "initial";
+  }
+});
